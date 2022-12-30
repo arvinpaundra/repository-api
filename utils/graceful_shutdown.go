318f28a56d9801373325f53e1f58dff0ecc697fe
arvinpaundra <arvinpaundra06@gmail.com>
2022-12-31 00:15:12 +0700

    perform graceful shutdown

---
+++ utils/graceful_shutdown.go
@@ -0,0 +1,64 @@
+package utils
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+type Operation func(context.Context) error
+
+// perform application to shutdown gracefully
+func GracefulShutdown(ctx context.Context, timeout time.Duration, ops map[string]Operation) <-chan struct{} {
+	wait := make(chan struct{})
+
+	go func() {
+		s := make(chan os.Signal, 1)
+
+		// add any other syscall that you want to be notified with
+		signal.Notify(s, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
+		<-s
+
+		log.Println("shutting down")
+
+		// set timeout for the ops to be done to prevent system hang
+		timeoutFunc := time.AfterFunc(timeout, func() {
+			log.Printf("timeout %d ms has been elapsed, force exit", timeout.Milliseconds())
+			os.Exit(0)
+		})
+
+		defer timeoutFunc.Stop()
+
+		var wg sync.WaitGroup
+
+		// do the operation synchronously to save time
+		for key, op := range ops {
+			wg.Add(1)
+			innerOp := op
+			innerKey := key
+
+			go func() {
+				defer wg.Done()
+
+				log.Printf("cleaning up: %v", innerKey)
+
+				if err := innerOp(ctx); err != nil {
+					log.Printf("%s: cleaning up failed: %s", innerKey, err.Error())
+					return
+				}
+
+				log.Printf("%s was shutdown gracefully", innerKey)
+			}()
+		}
+
+		wg.Wait()
+
+		close(wait)
+	}()
+
+	return wait
+}
