209c7d6eb919aca1a40cbffe659d921a5de5d197
arvinpaundra <arvinpaundra06@gmail.com>
2023-01-01 03:59:48 +0700

    feat(app): add domain factory category

--- drivers/domain_factory.go
+++ drivers/domain_factory.go
@@ -1,1 +1,10 @@
 package drivers
+
+import (
+	"github.com/arvinpaundra/repository-api/drivers/mysql/category"
+	"gorm.io/gorm"
+)
+
+func NewCategoryRepository(conn *gorm.DB) category.CategoryRepository {
+	return category.NewRepositorySQL(conn)
+}
