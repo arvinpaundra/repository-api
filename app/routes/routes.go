e01f70aa7377fb3a13801a5917b7a38a839734fc
arvinpaundra <arvinpaundra06@gmail.com>
2023-01-01 03:57:50 +0700

    feat(app): add category routes

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	controller "github.com/arvinpaundra/repository-api/controllers"
+	"github.com/arvinpaundra/repository-api/drivers"
+	service "github.com/arvinpaundra/repository-api/services"
 	"github.com/go-redis/redis/v8"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
@@ -12,4 +15,18 @@ type RouteConfig struct {
 	Redis *redis.Client
 }
 
-func (rc *RouteConfig) New() {}
+func (rc *RouteConfig) New() {
+	categoryRepository := drivers.NewCategoryRepository(rc.MySQl)
+	categoryService := service.NewCategoryService(categoryRepository)
+	categoryController := controller.NewCategoryController(categoryService)
+
+	// API current version
+	v1 := rc.Echo.Group("/api/v1")
+
+	// category routes
+	category := v1.Group("/categories")
+	category.POST("", categoryController.HandlerCreateCategory)
+	category.PUT("/:categoryId", categoryController.HandlerUpdateCategory)
+	category.GET("", categoryController.HandlerFindAllCategories)
+	category.GET("/:categoryId", categoryController.HandlerFindCategoryById)
+}
