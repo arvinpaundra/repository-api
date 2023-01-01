f4caefa446bac25680fc3f82e4527d400b3b9d90
arvinpaundra <arvinpaundra06@gmail.com>
2023-01-02 04:15:19 +0700

    feat(collection): add service handler collection

---
+++ services/collection_service.go
@@ -0,0 +1,16 @@
+package service
+
+import (
+	"context"
+
+	"github.com/arvinpaundra/repository-api/models/web/collection/request"
+	"github.com/arvinpaundra/repository-api/models/web/collection/response"
+)
+
+type CollectionService interface {
+	Create(ctx context.Context, collection request.CreateCollectionRequest) error
+	Update(ctx context.Context, collection request.UpdateCollectionRequest, collectionId string) error
+	Delete(ctx context.Context, collectionId string) error
+	FindAll(ctx context.Context, keyword string, limit int, offset int) ([]response.CollectionResponse, int, int, error)
+	FindById(ctx context.Context, collectionId string) (response.CollectionResponse, error)
+}
