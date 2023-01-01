760c71444b473ceed1fd707f6e6ae1a3846d3e9b
arvinpaundra <arvinpaundra06@gmail.com>
2023-01-02 04:14:16 +0700

    fix: add tag form name

--- models/web/category/request/update.go
+++ models/web/category/request/update.go
@@ -3,7 +3,7 @@ package request
 import "github.com/arvinpaundra/repository-api/models/domain"
 
 type UpdateCategoryRequest struct {
-	Name string `json:"name" validate:"required"`
+	Name string `json:"name" form:"name" validate:"required"`
 }
 
 func (req *UpdateCategoryRequest) ToDomainCategory() domain.Category {
