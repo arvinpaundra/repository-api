7bd03568329d16401bff685e70c579963e75a9ac
arvinpaundra <arvinpaundra06@gmail.com>
2023-01-03 12:34:41 +0700

    feat(departement): add repository handler departement

---
+++ drivers/mysql/departement/departement_repository.go
@@ -0,0 +1,16 @@
+package departement
+
+import (
+	"context"
+
+	"github.com/arvinpaundra/repository-api/models/domain"
+)
+
+type DepartementRepository interface {
+	Save(ctx context.Context, departement domain.Departement) error
+	Update(ctx context.Context, departement domain.Departement, departementId string) error
+	Delete(ctx context.Context, departementId string) error
+	FindAll(ctx context.Context, keyword string, limit int, offset int) ([]domain.Departement, int, error)
+	FindById(ctx context.Context, departementId string) (domain.Departement, error)
+	FindByProgramStudyId(ctx context.Context, programStudyId string) ([]domain.Departement, error)
+}
