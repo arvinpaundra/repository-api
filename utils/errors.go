35098d760d9fe3d27aeed021a698aa7d34ac859e
arvinpaundra <arvinpaundra06@gmail.com>
2023-04-30 01:38:14 +0700

    add errors for repository

--- utils/errors.go
+++ utils/errors.go
@@ -4,62 +4,89 @@ import "errors"
 
 var (
 	// error authentication failed
-	ErrAuthenticationFailed = errors.New("wrong email or password")
+	ErrAuthenticationFailed = errors.New("Wrong email or password")
 
 	// error user not found
-	ErrUserNotFound = errors.New("user not found")
+	ErrUserNotFound = errors.New("User not found")
 
 	// ErrCategoryNotFound error category not found
-	ErrCategoryNotFound = errors.New("category not found")
+	ErrCategoryNotFound = errors.New("Category not found")
 
 	// error collection not found
-	ErrCollectionNotFound = errors.New("collection not found")
+	ErrCollectionNotFound = errors.New("Collection not found")
 
 	// error role not found
-	ErrRoleNotFound = errors.New("role not found")
+	ErrRoleNotFound = errors.New("Role not found")
 
 	// error study program not found
-	ErrStudyProgramNotFound = errors.New("study program not found")
+	ErrStudyProgramNotFound = errors.New("Study program not found")
 
 	// error pemustaka not found
-	ErrPemustakaNotFound = errors.New("pemustaka not found")
+	ErrPemustakaNotFound = errors.New("Pemustaka not found")
+
+	// error examiner not found
+	ErrExaminerNotFound = errors.New("Examiner not found")
+
+	// error mentor not found
+	ErrMentorNotFound = errors.New("Mentor not found")
 
 	// error departement not found
-	ErrDepartementNotFound = errors.New("departement not found")
+	ErrDepartementNotFound = errors.New("Departement not found")
 
 	// error staff not found
-	ErrStaffNotFound = errors.New("staff not found")
+	ErrStaffNotFound = errors.New("Staff not found")
 
 	// error repository not found
-	ErrRepositoryNotFound = errors.New("repository not found")
+	ErrRepositoryNotFound = errors.New("Repository not found")
+
+	// error document of repository not found
+	ErrDocumentsNotFound = errors.New("Documents not found")
 
 	// error request access not found
-	ErrRequestAccessNotFound = errors.New("request access not found")
+	ErrRequestAccessNotFound = errors.New("Request access not found")
 
 	// error email already used
-	ErrEmailAlreadyUsed = errors.New("email already used")
+	ErrEmailAlreadyUsed = errors.New("Email already used")
 
 	// error email not found
-	ErrEmailNotFound = errors.New("email not found")
+	ErrEmailNotFound = errors.New("Email not found")
 
 	// error invalid password length
-	ErrInvalidPasswordLength = errors.New("minimum password length are 6 characters long")
+	ErrInvalidPasswordLength = errors.New("Minimum password length are 6 characters long")
 
 	// error token has been expired
-	ErrTokenExpired = errors.New("token has been expired")
+	ErrTokenExpired = errors.New("Token has been expired")
 
 	// error token has been sent
-	ErrTokenHasBeenSent = errors.New("token has been sent")
+	ErrTokenHasBeenSent = errors.New("Token has been sent")
 
 	// error token not match
-	ErrTokenNotMatch = errors.New("token not matched")
+	ErrTokenNotMatch = errors.New("Token not matched")
 
 	// error link has been expired
-	ErrLinkExpired = errors.New("link has been expired")
+	ErrLinkExpired = errors.New("Link has been expired")
 
 	// error waiting for acceptance to activate pemustaka account
-	ErrWaitingForAcceptance = errors.New("account is waiting for acceptance")
+	ErrWaitingForAcceptance = errors.New("Account is waiting for acceptance")
+
+	// error pemustaka already collected final project
+	ErrAlreadyCollectedFinalProject = errors.New("Pemustaka already collected final project")
+
+	// error pemustaka already collected internship report
+	ErrAlreadyCollectedInternshipReport = errors.New("Pemustaka already collected internship report")
+
+	// error fields are not equal
+	ErrFieldsAreNotEqual = errors.New("The fields must be equal")
 
 	// error internal server error
-	ErrInternalServerError = errors.New("internal server error")
+	ErrInternalServerError = errors.New("Internal server error")
+
+	// error invalid token header
+	ErrInvalidTokenHeader = errors.New("Invalid token header")
+
+	// error forbidden access
+	ErrForbiddenAccess = errors.New("Access forbidden")
+
+	// error unauthorized user
+	ErrUnAuthorized = errors.New("User unauthorized")
 )
