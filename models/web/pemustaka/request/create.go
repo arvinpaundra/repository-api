5c1507bd2d3177c0774aaecd25bb9301b9eaa286
arvinpaundra <arvinpaundra06@gmail.com>
2023-07-15 02:43:44 +0700

    feat(pemustaka): fix adding email fields

--- models/web/pemustaka/request/create.go
+++ models/web/pemustaka/request/create.go
@@ -7,7 +7,7 @@ type CreatePemustakaRequest struct {
 	DepartementId               string `json:"departement_id" form:"departement_id" validate:"required"`
 	StudyProgramId              string `json:"study_program_id" form:"study_program_id" validate:"required"`
 	Fullname                    string `json:"fullname" form:"fullname" validate:"required"`
-	Email                       string `json:"email" form:"email" validate:"required"`
+	Email                       string `json:"email" form:"email" validate:"required,email"`
 	Telp                        string `json:"telp" form:"telp"`
 	BirthDate                   string `json:"birth_date" form:"birth_date"`
 	Gender                      string `json:"gender" form:"gender"`
