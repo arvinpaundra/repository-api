bbb150ccbceb84a340fb568b1ca5ebec87518e17
arvinpaundra <arvinpaundra06@gmail.com>
2023-07-19 17:13:36 +0700

    chore: add validation input rules

--- helper/validate_request.go
+++ helper/validate_request.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"fmt"
 	"reflect"
 	"strings"
 
@@ -41,7 +42,7 @@ func validatorErrorMessage(validationError error) ValidationError {
 
 	// make error message for each invalid field
 	for _, err := range validationError.(validator.ValidationErrors) {
-		// append error message to the map, where the key is field name and value is an error desctiption
+		// append error message to the map, where the key is field name and value is an error description
 		errFields[err.Field()] = msgForTag(err)
 	}
 
@@ -51,10 +52,26 @@ func validatorErrorMessage(validationError error) ValidationError {
 func msgForTag(fieldError validator.FieldError) string {
 	switch fieldError.Tag() {
 	case "required":
-		return "Bagian in iwajib diisi"
+		return "Wajib diisi"
 	case "email":
 		return "Email tidak valid"
+	case "hexcolor":
+		return "Kode warna tidak valid"
+	case "base64rawurl":
+		return "Format harus berupa base64 raw url encoding"
+	case "min":
+		min := fieldError.Param()
+		return fmt.Sprintf("Minimal panjang %s karakter", min)
+	case "max":
+		max := fieldError.Param()
+		return fmt.Sprintf("Maksimal panjang %s karakter", max)
+	case "eqfield":
+		return "Input tidak sama"
+	case "numeric":
+		return "Hanya boleh berupa angka"
+	case "alpha":
+		return "Hanya boleh berupa huruf"
+	default:
+		return fieldError.Error()
 	}
-
-	return fieldError.Error()
 }
