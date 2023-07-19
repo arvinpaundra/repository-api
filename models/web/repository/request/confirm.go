1a0055727560d04158f309d8eb6a6c329fbc8078
arvinpaundra <arvinpaundra06@gmail.com>
2023-07-19 18:27:59 +0700

    resolve not saved conflict file

--- models/web/repository/request/confirm.go
+++ models/web/repository/request/confirm.go
@@ -1,10 +1,6 @@
 package request
 
 type ConfirmRequest struct {
-<<<<<<< HEAD
 	Status  string `json:"status" form:"status" validate:"required"`
 	Reasons string `json:"reasons" form:"reasons"`
-=======
-	Status string `json:"status" form:"status" validate:"required"`
->>>>>>> master
 }
