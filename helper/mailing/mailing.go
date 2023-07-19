fa5b7d7781b1e073ff1c853f8d57fe37930d036d
arvinpaundra <arvinpaundra06@gmail.com>
2023-07-19 16:59:55 +0700

    feat(app): refactor send mail handler

--- helper/mailing/mailing.go
+++ helper/mailing/mailing.go
@@ -1,20 +1,21 @@
 package mailing
 
 import (
+	"fmt"
 	"strconv"
 
-	"github.com/arvinpaundra/repository-api/configs"
 	"github.com/arvinpaundra/repository-api/helper"
 	"github.com/arvinpaundra/repository-api/templates"
 	"gopkg.in/gomail.v2"
 )
 
 type Mailing struct {
-	host       string
-	port       string
-	senderName string
-	email      string
-	password   string
+	host        string
+	port        string
+	senderName  string
+	email       string
+	password    string
+	frontendURL string
 }
 
 type User struct {
@@ -37,21 +38,40 @@ type Repository struct {
 }
 
 // NewMailing returns a new Mailing struct
-func NewMailing(host, port, email, password, senderName string) *Mailing {
+func NewMailing(host, port, email, password, senderName, frontendURL string) *Mailing {
 	return &Mailing{
-		host:       host,
-		port:       port,
-		email:      email,
-		password:   password,
-		senderName: senderName,
+		host:        host,
+		port:        port,
+		email:       email,
+		password:    password,
+		senderName:  senderName,
+		frontendURL: frontendURL,
 	}
+}
+
+func (mc *Mailing) send(email, subject, message string) error {
+	mail := gomail.NewMessage()
+	mail.SetHeader("From", mc.senderName)
+	mail.SetHeader("To", email)
+	mail.SetHeader("Subject", subject)
+	mail.SetBody("text/html", message)
+
+	port, _ := strconv.Atoi(mc.port)
+
+	dialer := gomail.NewDialer(mc.host, port, mc.email, mc.password)
+
+	if err := dialer.DialAndSend(mail); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (mc *Mailing) SendForgotPasswordMail(recipient, subject, token string) error {
 	data := map[string]interface{}{
 		"token":    token,
 		"email":    recipient,
-		"base_url": configs.GetConfig("FE_BASE_URL"),
+		"base_url": mc.frontendURL,
 	}
 
 	result, err := helper.RenderHTMLToString(templates.ForgotPassword, nil, data)
@@ -60,19 +80,7 @@ func (mc *Mailing) SendForgotPasswordMail(recipient, subject, token string) erro
 		return err
 	}
 
-	mail := gomail.NewMessage()
-	mail.SetHeader("From", mc.senderName)
-	mail.SetHeader("To", recipient)
-	mail.SetHeader("Subject", subject)
-	mail.SetBody("text/html", result)
-
-	port, _ := strconv.Atoi(mc.port)
-
-	dialer := gomail.NewDialer(mc.host, port, mc.email, mc.password)
-
-	err = dialer.DialAndSend(mail)
-
-	if err != nil {
+	if err := mc.send(recipient, subject, result); err != nil {
 		return err
 	}
 
@@ -80,139 +88,45 @@ func (mc *Mailing) SendForgotPasswordMail(recipient, subject, token string) erro
 }
 
 func (mc *Mailing) SendVerifiedRegisterMail(subject string, user User) error {
-	data := map[string]interface{}{
-		"fullname":        user.Fullname,
-		"email":           user.Email,
-		"identity_number": user.IdentityNumber,
-		"departement":     user.Departement,
-		"study_program":   user.StudyProgram,
-		"role":            user.Role,
-		"year_gen":        user.YearGen,
-		"base_url":        configs.GetConfig("FE_BASE_URL"),
+	var message string
+
+	if user.Role == "Mahasiswa" {
+		message = fmt.Sprintf("Hai, %s<br><br>Akun kamu <b>berhasil terverifikasi</b> dan sekarang menjadi bagian dari REKSI PNC. Kami ucapkan selamat datang di dunia pengetahuan tak terbatas!<br><br>Berikut adalah data yang kamu berikan :<br>Nama Lengkap : <b>%s</b><br>NIM : <b>%s</b><br>Jurusan : <b>%s</b><br>Program Studi : <b>%s</b><br>Alamat Email : <b>%s</b><br>Role : <b>%s</b><br>Tahun Angkatan : <b>%s</b><br><br>Salam hangat,<br>REKSI PNC", user.Fullname, user.Fullname, user.IdentityNumber, user.Departement, user.StudyProgram, user.Email, user.Role, user.YearGen)
+	} else {
+		message = fmt.Sprintf("Hai, %s<br><br>Akun kamu <b>berhasil terverifikasi</b> dan sekarang menjadi bagian dari REKSI PNC. Kami ucapkan selamat datang di dunia pengetahuan tak terbatas!<br><br>Berikut adalah data yang kamu berikan :<br>Nama Lengkap : <b>%s</b><br>NIM : <b>%s</b><br>Jurusan : <b>%s</b><br>Program Studi : <b>%s</b><br>Alamat Email : <b>%s</b><br>Role : <b>%s</b><br><br>Salam hangat,<br>REKSI PNC", user.Fullname, user.Fullname, user.IdentityNumber, user.Departement, user.StudyProgram, user.Email, user.Role)
 	}
 
-	result, err := helper.RenderHTMLToString(templates.VerifiedRegister, nil, data)
-
-	if err != nil {
-		return err
-	}
-
-	mail := gomail.NewMessage()
-	mail.SetHeader("From", mc.senderName)
-	mail.SetHeader("To", user.Email)
-	mail.SetHeader("Subject", subject)
-	mail.SetBody("text/html", result)
-
-	port, _ := strconv.Atoi(mc.port)
-
-	dialer := gomail.NewDialer(mc.host, port, mc.email, mc.password)
-
-	err = dialer.DialAndSend(mail)
-
-	if err != nil {
+	if err := mc.send(user.Email, subject, message); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (mc *Mailing) SendDeniedRegisterMail(recepient, subject, reasons string) error {
-	data := map[string]interface{}{
-		"reasons":  reasons,
-		"base_url": configs.GetConfig("FE_BASE_URL"),
-	}
+func (mc *Mailing) SendDeniedRegisterMail(subject, reasons string, user User) error {
+	message := fmt.Sprintf("Hai, %s<br><br>Dengan menyesal, kami ingin memberitahukan bahwa registrasi Anda telah <b>ditolak</b>. Setelah melakukan peninjauan, kami menemukan ketidaksesuaian dengan informasi yang Anda berikan.<br><br>Berikut adalah alasan penolakan registrasi Anda :<br>%s<br><br>Jika terdapat kekeliruan silahkan segera menghubungi pihak perpustakaan.<br><br>Salam hangat,<br>REKSI PNC", user.Fullname, reasons)
 
-	result, err := helper.RenderHTMLToString(templates.DeniedRegister, nil, data)
-
-	if err != nil {
-		return err
-	}
-
-	mail := gomail.NewMessage()
-	mail.SetHeader("From", mc.senderName)
-	mail.SetHeader("To", recepient)
-	mail.SetHeader("Subject", subject)
-	mail.SetBody("text/html", result)
-
-	port, _ := strconv.Atoi(mc.port)
-
-	dialer := gomail.NewDialer(mc.host, port, mc.email, mc.password)
-
-	err = dialer.DialAndSend(mail)
-
-	if err != nil {
+	if err := mc.send(user.Email, subject, message); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (mc *Mailing) SendVerifiedRepositoryMail(recepient, subject string, repository Repository) error {
-	data := map[string]interface{}{
-		"title":          repository.Title,
-		"authors":        repository.Authors,
-		"collection":     repository.Collection,
-		"category":       repository.Category,
-		"departement":    repository.Departement,
-		"date_validated": repository.DateValidated,
-		"base_url":       configs.GetConfig("FE_BASE_URL"),
-	}
+func (mc *Mailing) SendVerifiedRepositoryMail(subject string, user User, repository Repository) error {
+	message := fmt.Sprintf("Hai, %s<br><br>Horee, karya tulis ilmiah yang Anda unggah telah <b>disetujui</b>. Kami sangat menghargai kontribusi dan semangat Anda dalam berbagi pengetahuan.<br><br>Berikut adalah detail karya tulis ilmiah yang telah Anda unggah :<br>Judul : <b>%s</b><br>Penulis : <b>%s</b><br>Koleksi : <b>%s</b><br>Kategori : <b>%s</b><br>Jurusan : <b>%s</b><br>Tanggal Divalidasi : <b>%s</b><br><br>Salam hangat,<br>REKSI PNC", user.Fullname, repository.Title, repository.Authors, repository.Collection, repository.Category, repository.Departement, repository.DateValidated)
 
-	result, err := helper.RenderHTMLToString(templates.VerifiedRepository, nil, data)
-
-	if err != nil {
-		return err
-	}
-
-	mail := gomail.NewMessage()
-	mail.SetHeader("From", mc.senderName)
-	mail.SetHeader("To", recepient)
-	mail.SetHeader("Subject", subject)
-	mail.SetBody("text/html", result)
-
-	port, _ := strconv.Atoi(mc.port)
-
-	dialer := gomail.NewDialer(mc.host, port, mc.email, mc.password)
-
-	err = dialer.DialAndSend(mail)
-
-	if err != nil {
+	if err := mc.send(user.Email, subject, message); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (mc *Mailing) SendDeniedRepositoryMail(recepient, subject string, repository Repository) error {
-	data := map[string]interface{}{
-		"title":          repository.Title,
-		"authors":        repository.Authors,
-		"collection":     repository.Collection,
-		"category":       repository.Category,
-		"departement":    repository.Departement,
-		"date_validated": repository.DateValidated,
-		"base_url":       configs.GetConfig("FE_BASE_URL"),
-	}
+func (mc *Mailing) SendDeniedRepositoryMail(subject, reasons string, user User, repository Repository) error {
+	message := fmt.Sprintf("Hai, %s<br><br>Email ini ditujukan untuk memberitahukan bahwa unggahan karya tulis ilmiah Anda dengan judul <b>%s</b> telah <b>ditolak</b>. Setelah melakukan peninjauan, kami menemukan ketidaksesuaian pada karya tulis ilmiah yang Anda unggah.<br><br>Berikut adalah beberapa alasan penolakan unggahan karya tulis ilmiah Anda :<br>%s<br><br>Jika terdapat kekeliruan silahkan segera menghubungi pihak perpustakaan.<br><br>Salam hangat,<br>REKSI PNC<br>", user.Fullname, repository.Title, reasons)
 
-	result, err := helper.RenderHTMLToString(templates.DeniedRepository, nil, data)
-
-	if err != nil {
-		return err
-	}
-
-	mail := gomail.NewMessage()
-	mail.SetHeader("From", mc.senderName)
-	mail.SetHeader("To", recepient)
-	mail.SetHeader("Subject", subject)
-	mail.SetBody("text/html", result)
-
-	port, _ := strconv.Atoi(mc.port)
-
-	dialer := gomail.NewDialer(mc.host, port, mc.email, mc.password)
-
-	err = dialer.DialAndSend(mail)
-
-	if err != nil {
+	if err := mc.send(user.Email, subject, message); err != nil {
 		return err
 	}
 
